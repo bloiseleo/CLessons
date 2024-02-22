840be10f687a7358f654ea5daa2eaa0d039446a0
Leonardo Bloise <leonardocsbloise@gmail.com>
2024-02-21 22:59:22 -0300

    feature: forca game

--- main.c
+++ main.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void opening() {
+	printf("*******************\n");
+	printf("*  Jogo da Forca  *\n");
+	printf("*******************\n");
+	return;
+}
+
+char* createPlaceholders(char* word) {
+	char* placeholders = malloc(sizeof(char) * (strlen(word) + 1));
+	for(int i = 0; i < strlen(word); i++) {
+		placeholders[i] = '_';
+	}
+	placeholders[strlen(word) + 1] = '\0';
+	return placeholders;
+}
+
+void checkQuick(char* placeholders, char* secret, char quick) {
+	for(int i = 0; i < strlen(secret); i++) {
+		if(secret[i] == quick) {
+			placeholders[i] = quick;
+		}
+	}
+}
 
 int main() {
-	printf("Hello World");
+	char secret[] = "SECRET_WORD";
+	char* placeholders = createPlaceholders(secret);
+	int right = 0;
+	int killed = 0;
+	opening();
+	do {
+		printf("%s\n", placeholders);
+		char quick;
+		printf("Type your quick:");
+		fflush(stdout);
+		scanf(" %c", &quick);
+		printf("O seu chute foi: %c", quick);
+		checkQuick(placeholders, secret, quick);	
+		printf("\n");
+	} while(!killed && !right);
 	return 0;
 }
+
