508e5c12c23cd23b04762fa46ce4ff683f3335f6
Leonardo Bloise <leonardocsbloise@gmail.com>
2024-02-24 10:08:41 -0300

    fix: better handling of strings to avoid memory trash

--- main.c
+++ main.c
@@ -1,96 +1,124 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 #define MAX_TRIES 10
 
 void opening() {
-	printf("*******************\n");
-	printf("*  Jogo da Forca  *\n");
-	printf("*******************\n");
-	return;
+  printf("*******************\n");
+  printf("*  Jogo da Forca  *\n");
+  printf("*******************\n");
+  return;
 }
 
-char* createPlaceholders(char* word) {
-	char* placeholders = malloc(sizeof(char) * (strlen(word) + 1));
-	for(int i = 0; i < strlen(word); i++) {
-		placeholders[i] = '_';
-	}
-	placeholders[strlen(word) + 1] = '\0';
-	return placeholders;
+char *createPlaceholders(char *word) {
+  char *placeholders = malloc(sizeof(char) * strlen(word) + 1);
+  int i;
+  for (i = 0; word[i] != '\0'; i++) {
+    placeholders[i] = '_';
+  }
+  placeholders[i] = '\0';
+  return placeholders;
 }
 
-int checkQuick(char* placeholders, char* secret, char quick) {
-	int changed = 0;
-	for(int i = 0; i < strlen(secret); i++) {
-		if(secret[i] == quick) {
-			changed++;
-			placeholders[i] = quick;
-		}
-	}
-	return changed;
+int checkQuick(char *placeholders, char *secret, char quick) {
+  int changed = 0;
+  for (int i = 0; i < strlen(secret); i++) {
+    if (secret[i] == quick) {
+      changed++;
+      placeholders[i] = quick;
+    }
+  }
+  return changed;
 }
 
-int checkWin(char* placeholders, char* word) {
-	int equals = 0;
-	for(int i = 0; i < strlen(word); i++) {
-		if(word[i] == placeholders[i]) {
-			equals++;	
-		}
-	}
-	return equals == strlen(word);
+int checkWin(char *placeholders, char *word) {
+  int equals = 0;
+  for (int i = 0; i < strlen(word); i++) {
+    if (word[i] == placeholders[i]) {
+      equals++;
+    }
+  }
+  return equals == strlen(word);
 }
 
-void drawForca(char* placeholders, char * secret, int* tries) {
-	printf("%s\n", placeholders);
-	char quick;
-	printf("Type your quick:");
-	scanf(" %c", &quick);
-	printf("O seu chute foi: %c", quick);
-	if(checkQuick(placeholders, secret, quick) == 0) {
-		(*tries)++;
-	}
+void drawForca(char *placeholders, char *secret, int *tries) {
+  printf("%s\n", placeholders);
+  char quick;
+  printf("Type your quick:");
+  scanf(" %c", &quick);
+  printf("O seu chute foi: %c", quick);
+  if (checkQuick(placeholders, secret, quick) == 0) {
+    (*tries)++;
+  }
 }
 
-char* chooseSecretWord() {
-	int qtd;	
-	char* secret = malloc(sizeof(char));
-	FILE* f = fopen("palavras.txt", "r");
-	fscanf(f, "%d", &qtd);
-	qtd++;
-	srand(time(0));
-	int random = rand() % qtd;
-	if(random == 0) random = 1;
-	for(int i = 0; i < random; i++) {
-		fscanf(f, "%s", secret);
-	}
-	fclose(f);
-	return secret;
-}	
+char *readline(FILE *fp) {
+  int bufferSize = 1024;
+  char *buffer = malloc(sizeof(char) * bufferSize);
+  int i = 0;
+  char c;
+  do {
+    c = fgetc(fp);
+    if (c == '\n') {
+      break;
+    }
+    if (i >= bufferSize) {
+      bufferSize *= 2;
+      buffer = realloc(buffer, sizeof(char) * (bufferSize));
+    }
+    buffer[i++] = c;
+  } while (c != EOF);
+  char *str = realloc(buffer, sizeof(char) * (i + 1));
+  str[i] = '\0';
+  return str;
+}
+
+int generateRandomInt(int max) {
+  srand(time(0));
+  int r = rand() % (max + 1);
+  return r == 0 ? 1 : r;
+}
+
+char *chooseSecretWord() {
+  int qtd;
+  char *secret;
+  FILE *f = fopen("palavras.txt", "r");
+  fscanf(f, " %d", &qtd);
+  fseek(f, 1, SEEK_CUR);
+  int selected = generateRandomInt(qtd);
+  int i = 0;
+  while (!feof(f) && i < selected) {
+    char *line = readline(f);
+    secret = line;
+    i++;
+  }
+  fclose(f);
+  return secret;
+}
 
 int main() {
-	char* secret = chooseSecretWord();
-	char* placeholders = createPlaceholders(secret);
-	int right = 0;
-	int killed = 0;
-	int tries = 0;
-	opening();
-	do {
-		drawForca(placeholders, secret, &tries);
-		if(checkWin(placeholders, secret) > 0) {
-			right = 1;
-		}
-		if(tries >= MAX_TRIES) {
-			killed = 1;
-		}
-		printf("\n");
-	} while(!killed && !right);
-	if(right > 0 && killed == 0) {
-		printf("You won!\n");
-	} else {
-		printf("You lose!\n");
-	}
-	return 0;
+  char *secret = chooseSecretWord();
+  char *placeholders = createPlaceholders(secret);
+  int right = 0;
+  int killed = 0;
+  int tries = 0;
+  opening();
+  do {
+    drawForca(placeholders, secret, &tries);
+    if (checkWin(placeholders, secret) > 0) {
+      right = 1;
+    }
+    if (tries >= MAX_TRIES) {
+      killed = 1;
+    }
+    printf("\n");
+  } while (!killed && !right);
+  if (right > 0 && killed == 0) {
+    printf("You won!\n");
+  } else {
+    printf("You lose!\n");
+  }
+  return 0;
 }
-
