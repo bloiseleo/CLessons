a459c084f06c60d39894f8467c7cb5de392d3595
Leonardo <leonardocsbloise@gmail.com>
2024-02-26 00:28:32 -0300

    feat: reading map

--- main.c
+++ main.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "filereading.h"
 
 int main() {
-    FILE* f = fopen("./exemplo.txt", "r");
-    char* line;
-    while ((line = readline(f)) != NULL) {
-        printf("Linha: %s\n", line);
+    char mapa[5][11];
+    FILE* f = fopen("./mapa.txt", "r");
+
+    if(f == NULL) {
+        printf("Erro ao ler o Mapa\n");
+        exit(1);
     }
+
+    for (int i = 0; i < 5; i++) {
+        char* line = readline(f);
+        if(line == NULL) {
+            printf("Erro ao ler linha do Mapa\n");
+            exit(1);
+        }
+        sprintf(mapa[i], "%s", line);
+    }
+    
+    for (int i = 0; i < 5; i++) {
+        printf("%s\n", mapa[i]);
+    }
+    
+
     return 0;
 }
